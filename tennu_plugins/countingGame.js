fc18378241e81dcaf1c6e94a112a7002fc5decaa
codeninjathe <sponge7226@gmail.com>
2015-04-16 23:15:20 -0500

    debugging the %setEnd command
    Basically the title.

--- tennu_plugins/countingGame.js
+++ tennu_plugins/countingGame.js
@@ -1,29 +1,41 @@
 // Initialization of the node module.
 var random = Math.random;
 var round = Math.round;
-const function contains(a, obj) {
-    var i = a.length;
-    while (i--) {
-       if (a[i] === obj) {
-           return true;
-       }
-    }
-    return false;
-}
-const function isNums(str){
-    return int(str.replace(/[0-9]/g, "").length) == 0;
-}
+// function contains(a, obj) {
+//     var i = a.length;
+//     while (i--) {
+//        if (a[i] === obj) {
+//            return true;
+//        }
+//     }
+//     return false;
+// }
+// const function isNums(str){
+//     return int(str.replace(/[0-9]/g, "").length) == 0;
+// }
 var countingGame = {
     init: function (client, imports) {
         var counter = 0;
         var stop = 10;
         var limit = 10;
-        var[] adminUsers = {
+        var adminUsers = [
             "akainocode",
             "Havvy",
             "Nyanta",
             "TsundereVanilla"
             //More can be added later
+        ];
+        function contains(a, obj) {
+           var i = a.length;
+             while (i--) {
+               if (a[i] === obj) {
+                   return true;
+                }
+            }
+            return false;
+        };
+        function isNums(str){
+           return int(str.replace(/[0-9]/g, "").length) == 0;
         }
         return {
             handlers: {
@@ -37,39 +49,50 @@ var countingGame = {
                     }
                     counter = counter + roll;
                     client.say(command.channel, roll + " was rolled by " + command.nickname +", and the counter is at " + counter);
-                    if(counter == stop){
+                    if(counter = stop - 1){
+                        client.say(command.channel, "HA. ALL OF YOU GUYS LOSE. THANK " + command.nickname + " FOR IT.");
+                    }
+                    else if(counter <= stop){
                         client.say(command.channel, "Congratulations, " + command.nickname + " has completed the counter up to " + counter + "!");
                         counter = 0;
                     }
-                 }
+                 },
                 '!setEnd': function(command) {
+                   
                     if(contains(adminUsers, command.nickname)){
-                        var[] params = command.args;
+                        var params = command.args;
                         if(params.length < 1)
-                            client.say("Invalid command: Not enough parameters")
+                            client.say(command.channel, "Invalid parameter: Not enough parameters");
                         else{
                             if(isNums(params[0])){
                                 stop = int(params[0]);
-                                client.say("Limit for the counting game is is successfully set to " + params[0]);
+                                client.say(command.channel, "Limit for the counting game is is successfully set to " + params[0]);
+                            }
+                            else{
+                                client.say(command.channel, "Invalid parameter: Parameter is not a number.")
                             }
 
                         }
                     }
                     else{
-                        client.say("Insufficient Permissions");
+                        client.say(command.channel, "Insufficient Permissions");
                     } 
                 }
              },
 
             help: {
                 'command': [
-                    '!counter',
+                    '!count',
                     ' ',
-                    'A small counting game in which you try to hit the limit.'
+                    'A small counting game in which you try to hit the limit.',
+                    '!setEnd',
+                    ' ',
+                    'A command intended to set the end of the counting game, only certain people can do this command.'
                 ]
+
             },
 
-            commands: ['countingGame']
+            commands: ['count', 'setEnd']
         };
     }
 };
