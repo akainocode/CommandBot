d49181029f75ba7bacce89b9307f9990189f0289
codeninjathe <sponge7226@gmail.com>
2015-04-16 20:10:53 -0500

    This is a bunch of stuff
    Yeah, the title

---
+++ node_modules/tennu/tennu_plugins/user.js
@@ -0,0 +1,24 @@
+const Promise = require('bluebird');
+const format = require('util').format;
+module.exports = {
+    init: function (client, imports) {
+        const isIdentifiedAs = function (nickname, accountname) {
+            client.debug('PluginUser', format('isIdentifiedAs(%s, %s)', nickname, accountname));
+            return client.whois(nickname).then(function (result) {
+                return result.map(function (whoisInfo) {
+                    if (whoisInfo.identified) {
+                        return whoisInfo.identifiedas.toLowerCase() === accountname.toLowerCase();
+                    } else {
+                        return false;
+                    }
+                }).unwrapOr(false);
+            }, function (err) {
+                client.error('PluginUser', err);
+                client.error('PluginUser', err.stack);
+                return false;
+            });
+        };
+        return { exports: { isIdentifiedAs: isIdentifiedAs } };
+    }
+};
+//# sourceMappingURL=../sourcemaps/plugin/user.js.map
---
+++ run.bat
@@ -0,0 +1,2 @@
+node ./node_modules/.bin/tennu config.json -v
+PAUSE
